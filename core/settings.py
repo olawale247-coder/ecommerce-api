1712098eb64a531c29672906a1133fbed534b7c3
nanya247 <myx4olawale@yahoo.com>
2026-07-21 13:48:50 +0100

    Added host

--- core/settings.py
+++ core/settings.py
@@ -23,7 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-01y2(#!+zf%b760(4kt$sxk&ps#0v95hk=sxubo3-c6sl998k='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
+SECURE_SSL_REDIRECT = False 
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+CSR_TRUSTED_ORIGINS = ['https://ecommerce-api-qk6s.onrender.com']
+
 import os
 
 ALLOWED_HOSTS = [
